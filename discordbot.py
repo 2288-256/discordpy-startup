b9cf33de2a3a7fc096a227b179ed4a1572521364
2288-256 <74705442+2288-256@users.noreply.github.com>
2021-02-06 10:26:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,29 +1,27 @@
 from discord.ext import commands
 import os
 import traceback
+import discord
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
+# 起動時に動作する処理
 @client.event
 async def on_ready():
-    ch_id = 677346242426044449
-    #入力されたチャンネルIDからチャンネル情報を取得してchannelに格納する。
-    channel = client.get_channel(ch_id)
-    #channelに指定されたワードをポストする。
-    await channel.send("起動完了！")
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
