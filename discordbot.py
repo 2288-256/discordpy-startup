a8c105dc0b52e8d375320aca4afa614c5d120dca
2288-256 <74705442+2288-256@users.noreply.github.com>
2021-02-06 10:18:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,15 +5,7 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_ready():
-#ココに記述
-#任意のチャンネルIDを記述
-ch_id = 677346242426044449
-#入力されたチャンネルIDからチャンネル情報を取得してchannelに格納する。
-channel = client.get_channel(ch_id)
-#channelに指定されたワードをポストする。
-await channel.send("起動完了！")
+
 
 @bot.event
 async def on_command_error(ctx, error):
